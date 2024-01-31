47e70a3fee17132d71404c6f67da8c3e42d891af
Chel <cdeszz8@gmail.com>
2024-01-30 21:29:07 -0600

    moved py

--- ARDUINO/reset_servos_to_0_pin2/reset_servos_to_0_pin2.ino
+++ ARDUINO/reset_servos_to_0_pin2/reset_servos_to_0_pin2.ino
@@ -2,7 +2,6 @@
 Servo miServo;  // Crear un objeto de la clase Servo para controlar el servomotor
 
 void setup() {
-  // put your setup code here, to run once:
   miServo.attach(7);  // El pin al que está conectado el servo (cámbialo si es necesario)
   
 }
@@ -10,6 +9,6 @@ void setup() {
 void loop() {
   miServo.write(0);
   delay(1000);  
-  miServo.write(180);
-  delay(1000);
+  //miServo.write(180);
+  //delay(1000);
 }
--- python/open_edge_docus.py
+++
@@ -1,7 +0,0 @@
-import os
-os.system("start \"\" http://myrobotnigel.blogspot.com/p/blog-page_7.html")
-os.system("start \"\" https://www.thingiverse.com/thing:3779525/files")
-os.system("start \"\" https://es.aliexpress.com/item/32240916087.html?spm=a2g0o.productlist.main.1.174d3W923W92k8&algo_pvid=77ab48cb-dd27-4a45-bb67-a1222977b21d&algo_exp_id=77ab48cb-dd27-4a45-bb67-a1222977b21d-0&pdp_npi=4%40dis%21MXN%21483.12%21391.29%21%21%2126.41%21%21%402103250d16970017320744987e69c2%2164273207789%21sea%21MX%210%21AB&curPageLogUid=z2JAH3YwUe7b")
-os.system("start \"\" https://www.geeksforgeeks.org/opencv-python-tutorial/")
-os.system("start \"\" https://inmoov.fr/hand-and-forarm/")
-os.system("start \"\" https://inmoov.fr/inmoov2-in-nixie/")
